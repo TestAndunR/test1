a5981891185a822e4d0583ce3d03d9ffc2f773f2
TestAndunR <andunranmal@gmail.com>
2018-10-30 15:51:08 +0530

    add promotion ddb

--- test1/lambda.js
+++ test1/lambda.js
@@ -1,4 +1,14 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+    ddb.scan({
+        TableName: 'Promotions'
+    }).promise().then(function (data) {
+        //your logic goes here
+    }).catch(function (err) {
+        //handle error
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }

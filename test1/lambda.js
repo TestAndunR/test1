bf6a135cb8a81ada13e256258360f13731733dd3
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2019-01-28 15:15:21 +0530

    cccc

--- test1/lambda.js
+++ test1/lambda.js
@@ -9,6 +9,6 @@ exports.handler = function (event, context, callback) {
     }).catch(function (err) {
         //handle error
     });
-
+xassasa
     callback(null, { "message": "Successfully executed" });
 }

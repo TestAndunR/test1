d3b4f7119c50487fd0b77095b5d6418a9a9750c8
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2019-02-12 13:27:56 +0530

    hhhh

--- test1/lambda3.js
+++ test1/lambda3.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
-    console.log("Yeas");
+    console.log("Yeashhhhhhhh");
     
     callback(null, {"message": "Successfully executed"});
 }
